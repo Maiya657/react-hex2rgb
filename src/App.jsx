53b3028c3fb66d5c66b30975d89f7ac7b221389f
Mayia Rodina <designw657@gmail.com>
2026-01-30 22:39:56 +0300

    fix HexToRgbConverter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react';
 import './App.css';
 import HexToRgbConverter from './Components/HexToRgbConverter/HexToRgbConverter';
 
 function App() {
-  const [color, setColor] = useState(undefined);
-
   return (
-    <div className="container" style={{backgroundColor: color}}>
-      <HexToRgbConverter setBackgroundColor={setColor} />
-    </div>
+    <HexToRgbConverter />
   )
 }
 
--- src/Components/HexToRgbConverter/HexToRgbConverter.jsx
+++ src/Components/HexToRgbConverter/HexToRgbConverter.jsx
@@ -1,36 +1,54 @@
 import { useState } from 'react';
 import hexToRgb from '../../helpers/hexToRgb';
 
-export function HexToRgbConverter(props) {
-  const { setBackgroundColor } = props;
-  const [ rgbText, setRgb ] = useState('rgb()')
+export function HexToRgbConverter() {
+  const [ rgbText, setRgb ] = useState('rgb()');
+  const backgroundColorDefault = 'rgb(230, 225, 225)';
+  const [color, setColor] = useState(backgroundColorDefault);
+  const [colorField, setColorField] = useState('');
 
   const handleColor = (e) => {
-    const hex = e.target.value
+    e.preventDefault();
+    const hex = e.target.value;
     
-    if (/^#.{6}$/.test(hex)) {
-      if (/^#[0-9A-Fa-f]{6}$/.test(hex)) {
-        const rgbRaw = hexToRgb(hex);
-        const rgb = `rgb(${rgbRaw.r}, ${rgbRaw.g}, ${rgbRaw.b})`
-        setRgb(rgb);
-        setBackgroundColor(rgb);
-      } else {
+    if (hex.length > 7) {
+      return;
+    }
+
+    setColorField(hex);
+ 
+    const setCorrectColor = (hex) => {
+      const rgbRaw = hexToRgb(hex);
+      const rgb = `rgb(${rgbRaw.r}, ${rgbRaw.g}, ${rgbRaw.b})`
+      setRgb(rgb);
+      setColor(rgb);
+    }
+    
+    switch (true) {
+      case (hex === ''): 
+        setColor(backgroundColorDefault);
+        break;
+      case (/^#[0-9A-Fa-f]{6}$/.test(hex)): 
+        setCorrectColor(hex);
+        break;
+
+      default: 
         setRgb('Ошибка!');
-        setBackgroundColor('rgb(196, 48, 48)');
-      } 
-    } else if (hex === '') {
-      setBackgroundColor(undefined);
+        setColor('rgb(196, 48, 48)');
+        break;
     }
   };
   
   return (
-    <div className='form-container'>
-      <form className='form'>
-        <label htmlFor='hex'></label>
-        <input type='text' id='hex' className='hex' onChange={handleColor} placeholder="#код цвета" />
-      </form>
-      <div className='rgb'>
-        <div className='rgb-text'>{rgbText}</div>
+    <div className="container" style={{backgroundColor: color}}>
+      <div className='form-container'>
+        <form className='form'>
+          <label htmlFor='hex'></label>
+          <input type='text' id='hex' className='hex' value={colorField} onChange={handleColor} placeholder="#код цвета" />
+        </form>
+        <div className='rgb'>
+          <div className='rgb-text'>{rgbText}</div>
+        </div>
       </div>
     </div>
   )
